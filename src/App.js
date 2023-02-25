baed6fee3bd933549bfd35c788a105e2597acb03
Dionisius Jordi <dionnando01@gmail.com>
2023-02-25 17:14:42 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Komponen from "./Komponen";
+import FunctionalComponent from "./Komponen/FunctionalComponent.jsx";
+
+function App() {
+    return (
+        <div className="App">
+            <Komponen />
+            <FunctionalComponent />
+        </div>
+    );
+}
+
+export default App;
